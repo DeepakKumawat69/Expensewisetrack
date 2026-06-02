3ec0a9e824a2f09d5c96b9c31bef6d4e3d9a3ddb
DeepakKumawat69 <deepakumawat659@gmail.com>
2026-06-02 21:44:01 +0530

    Your commit message here

--- .js
+++ .js
@@ -1,3 +1,11 @@
 // Hide welcome, show expense tracker
 document.getElementById('welcomePage').classList.remove('active');
 document.getElementById('expensePage').classList.add('active');
+
+// Hide welcome, show expense tracker
+document.getElementById('welcomePage').classList.remove('active');
+document.getElementById('expensePage').classList.add('active');// Hide welcome, show expense tracker
+document.getElementById('welcomePage').classList.remove('active');
+document.getElementById('expensePage').classList.add('active');// Hide welcome, show expense tracker
+document.getElementById('welcomePage').classList.remove('active');
+document.getElementById('expensePage').classList.add('active');
